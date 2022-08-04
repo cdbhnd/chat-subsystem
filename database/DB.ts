1a2cf0c03465f5e67658875ec5d50162bb597aea
Nikola Petrovic <nikola.petrovic@codebehind.rs>
2022-08-04 10:28:37 +0200

    db connection update

--- database/DB.ts
+++ database/DB.ts
@@ -6,7 +6,17 @@ export class DB {
     public static db: any;
 
     public static async init(): Promise<boolean> {
-        DB.db = await DB.dbDriver.MongoClient.connect("mongodb://" + config.get("mongoDbSettings.dbUser") + ":" + config.get("mongoDbSettings.dbPassword") + "@" + config.get("mongoDbSettings.dbHost"));
+        let connectionAuth = "";
+        try {
+            const user = config.get("mongoDbSettings.dbUser");
+            const pass = config.get("mongoDbSettings.dbPassword");
+            if (!!user && !!pass) {
+                connectionAuth = config.get("mongoDbSettings.dbUser") + ":" + config.get("mongoDbSettings.dbPassword") + "@";
+            }
+        } catch (e) {
+            console.log("NO DB AUTH");
+        }
+        DB.db = await DB.dbDriver.MongoClient.connect("mongodb://" + connectionAuth + config.get("mongoDbSettings.dbHost"));
         return true;
     }
 }
